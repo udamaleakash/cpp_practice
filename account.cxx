d8b91d1b544ff370a45923724475f54cacfe3cf6
kimaya <rasikaghadge04@gmail.com>
2026-06-15 18:51:47 +0530

    changes

--- account.cxx
+++ account.cxx
@@ -1,54 +1,21 @@
-
 #include <iostream>
-#include <string.h>
-
 using namespace std;
 
-class Account{
-    
-    private:
-        char *data;
-    public:
-    Account(const char *str)// constructor
+class test
+{
+    int a = 20;
+    int b = 40;
+
+public:
+    void show()
     {
-        data = new char [strlen(str)+1];
-        strcpy(data, str);
-        cout<<"constructor callled\n";
-    }
-    Account(const Account &other){
-        data = new char[strlen(other.data)+1];
-        strcpy(data, other.data);
-        cout<<"copy constructor callled\n";
-    }
-    
-    Account &operator=(const Account &other)
-    {
-        cout<<"copy Assignment operator callled\n";
-        if(this != &other)
-        {
-           
-            data = new char[strlen(other.data)+1];
-            strcpy(data, other.data);
-        }
-        return *this;
-    }
-    
-    ~Account()
-    {
-        cout<<"Destructor called\n";
-        delete[] data;
-        
+        cout << "a= " << a << endl;
+        cout << "b= " << b << endl;
     }
 };
 
 int main()
 {
-    Account a1("Hello");
-    Account a2 = a1;
-    
-    Account a3("Akash");
-    a2 = a3;
-    
-
-    return 0;
+    test t1;
+    t1.show();
 }
