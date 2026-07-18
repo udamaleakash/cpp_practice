f08322974c746df2bf3f2eb13fde9cc8a0114ce4
udamaleakash <udamaleakash323@gmail.com>
2026-07-18 14:33:30 +0530

    c

---
+++ LeetCode/18_july.cxx
@@ -0,0 +1,22 @@
+/*
+Q1.
+nums = [2,7,11,15]
+target = 9
+
+Output:
+[0,1]
+*/
+#include <iostream>
+using namespace std;
+int main()
+{
+    int nums[] = {2, 7, 11, 15};
+    int target = 9;
+
+    for (int i = 0; i < 4; i++)
+    {
+        
+    }
+
+    return 0;
+}
