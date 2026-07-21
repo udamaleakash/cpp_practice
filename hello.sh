7b9d7a61964836a9d793c24b2832e3fd1980c95f
udamaleakash <udamaleakash323@gmail.com>
2026-07-21 18:58:19 +0530

    c

---
+++ hello.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# echo "Hello, World!"
+# echo "Current User: $USER"
+# echo "Current Directory: $(pwd)"
+# echo "Today's Date: $(date)"
+# echo "Hi my name is $USER and I am currently in the directory $(pwd)  ."
+# echo "Compiling C++ code..."
+# g++ -o code_que_answer01 code_que_answer01.cxx
+
+# name="Akash"
+# echo $name
+
+read nameee
+echo "Hello, $nameee!"
