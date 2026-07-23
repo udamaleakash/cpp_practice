e089bbcecfe7eacdf6c25de259b9657de26437d5
udamaleakash <udamaleakash323@gmail.com>
2026-07-23 12:57:28 +0530

    c

--- LeetCode/18_july.cxx
+++ LeetCode/18_july.cxx
@@ -8,11 +8,9 @@ Output:
 */
 #include <iostream>
 using namespace std;
-int main()
-{
-    int nums[] = {1, 2, 11, 15, 7};
-    int target = 9;
 
+void result(int nums[], int target)
+{
     for (int i = 0; i < 5; i++)
     {
         for (int j = i + 1; j < 5; j++)
@@ -21,6 +19,38 @@ int main()
                 cout << i << " " << j << endl;
         }
     }
+}
+using namespace std;
+int main()
+{
+    int nums[] = {1, 2, 11, 15, 7};
+    int target = 9;
+
+    result(nums, target);
 
     return 0;
 }
+
+
+/*
+GDB commands
+g++ -g 18_july.cxx -o 18_july
+gdb ./18_july
+run
+break main
+next
+print nums
+print target
+next
+break result
+next
+print i
+list - current line
+info breakpoints
+info locals
+info args
+continue
+step
+delete 1 2 - delete breakpoints 1 and 2
+quit
+ */
